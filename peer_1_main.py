16fe788584479f137b80c793d45f4d2dd1d247e1
DennisA <dennis.apter@awacademy.training>
2022-11-08 15:21:22 +0200

    validations done

--- peer_1_main.py
+++ peer_1_main.py
@@ -20,8 +20,9 @@ def insert_into_timetable(start_date, start_time, end_date, end_time, project, t
         con.commit()
         # print 
         cursor.close()
+        return 'The hours were successfully inserted into the timetable'
     except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
+        return f'The hour were not inserted into the timetable, {error}'
     finally:
         if con is not None:
             con.close()
@@ -30,14 +31,63 @@ def insert_into_timetable(start_date, start_time, end_date, end_time, project, t
 
 
 if __name__ == "__main__":
-        # start_date = input("Please insert the start date (yyyy-mm-dd): ")
-        # start_time = input("Please insert the start time (00:00:00): ")
-        # end_date = input("Please insert the ending date (yyyy-mm-dd): ")
-        # end_time = input("Please insert the end time (00:00:00): ")
-        # project = input("Please insert the project the work is aimed at: ")
-        # tasks = input("Please insert the tasks worked on: ")
+    while True:
+        print ("""
+        1.Insert work hours into the timetable
+        2.Exit/Quit
+        """)
+        ans=input("What would you like to do? ") 
+        if ans=="1": 
+            print("\nAdd work hours")
 
-        insert_into_timetable('22-08-11', '09:00:00', '22-08-11', '12:00:00', 'project 1', 'github')
+            start_date = input("Please insert the start date (yy-mm-dd): ")
+            try:
+                validtime = datetime.strptime(start_date, "%y-%m-%d")
+            except ValueError:
+                print(f"Invalid time format. Insert: (yy-mm-dd)")
+                continue
+            
+            start_time = input("Please insert the start time (00:00:00): ")
+            try:
+                validtime = datetime.strptime(start_time, "%H:%M:%S")
+            except ValueError:
+                print(f"Invalid time format. Insert: (00:00:00)")
+                continue
+            
+            end_date = input("Please insert the ending date (yy-mm-dd): ")
+            try:
+                validtime = datetime.strptime(end_date, "%y-%m-%d")
+            except ValueError:
+                print(f"Invalid time format. Insert: (yy-mm-dd)")
+                continue
+            if datetime.strptime(end_date, "%y-%m-%d") < datetime.strptime(start_date, "%y-%m-%d"):
+                print("End date cannot be before start date. Please insert the ending date again.")
+                continue
+            
+            end_time = input("Please insert the end time (00:00:00): ")
+            try:
+                validtime = datetime.strptime(end_time, "%H:%M:%S")
+            except ValueError:
+                print(f"Invalid time format. Insert: (00:00:00)")
+                continue
+
+            project = input("Please insert the project the work is aimed at: ")
+            tasks = input("Please insert the tasks worked on: ")
+
+            print('')
+
+            print(insert_into_timetable(start_date, start_time, end_date, end_time, project, tasks))
+
+        elif ans=="2":
+            print("\nGoodbye") 
+            break
+        elif ans !="":
+            print("\n Not Valid Choice Try again") 
+
+
+        
+
+            
 
 
 
